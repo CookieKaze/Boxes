2a9eab6c25f8ef3008e29ce30968566f330ade49
Erin Luu <luuerin@gmail.com>
2016-11-01 11:09:59 -0400

    Finished setting up first part of Box class

---
+++ Box.h
@@ -0,0 +1,21 @@
+//
+//  Box.h
+//  Boxes
+//
+//  Created by Erin Luu on 2016-11-01.
+//  Copyright © 2016 Erin Luu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Box : NSObject
+
+//height, width, and length
+
+@property float height;
+@property float width;
+@property float length;
+
+
+
+@end
---
+++ Box.m
@@ -0,0 +1,29 @@
+//
+//  Box.m
+//  Boxes
+//
+//  Created by Erin Luu on 2016-11-01.
+//  Copyright © 2016 Erin Luu. All rights reserved.
+//
+
+#import "Box.h"
+
+@implementation Box
+
+- (instancetype) initWithWidth: (float)width andHeight: (float)height andLength: (float)length
+{
+    self = [super init];
+    if (self) {
+        _width = width;
+        _length = length;
+        _height = height;
+    }
+    return self;
+}
+
+
+- (float) calculateVolume {
+    float volume = self.width * self.height * self.length;
+    return volume;
+}
+@end
